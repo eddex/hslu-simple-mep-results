d5a9ebf42bb5375c61266aeb6587386dcce828e6
Brian <brianbossdennis@gmail.com>
2020-03-06 16:40:06 +0100

    refactor so the messages are only requests once

--- src/components/i18n.js
+++ src/components/i18n.js
@@ -1,12 +1,9 @@
 /**
- * Component for localization functions.
- */
-
-/**
  * Returns the language of MyCampus
  * @returns {string}
  */
-const getMyCampusLanguage = () => {
+const getLanguage = () => {
+
     languageLinks = document.getElementsByClassName("languagelink");
     for (let i = 0; i < languageLinks.length; i++) {
         const languageLink = languageLinks[i];
@@ -22,22 +19,38 @@ const getMyCampusLanguage = () => {
     else {
         return (chrome.runtime.getManifest()).default_locale
     }
+
 }
 
+let messages;
+
+
+/**
+ * Component for localization functions.
+ */
 const i18n = {
+
+    /**
+     * Populates the MyCampus informations
+     * @param {string} text to put into the cell.
+     */
+    init: async () => {
+
+        const language = getLanguage();
+
+        messages = await fetch(Helpers.getExtensionInternalFileUrl('_locales/' + language + '/messages.json'))
+            .then(response => response.json())
+    },
 
     /**
      * Returns the localized message
      * @param {string} message wo should be localized
      */
-    getMessage: async (message) => {
-        const language = getMyCampusLanguage();
-        const messages = await fetch(Helpers.getExtensionInternalFileUrl('_locales/' + language + '/messages.json'))
-            .then(response => response.json())
+    getMessage: (message) => {
         if (message in messages) {
             return messages[message].message
         }
         return undefined
     },
-}
 
+}
